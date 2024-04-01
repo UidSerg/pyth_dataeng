b477a8356e3ed322102e386a64f89782ea8fe784
Serg <uidserg@live.com>
2024-04-01 22:57:14 +0500

    sem часть 1

--- task1.py
+++ task1.py
@@ -1,1 +1,19 @@
-print("Hello world")
+
+"""
+Посчитайте сумму чётных элементов от 1 до n исключая кратные e. 
+Используйте while и if. 
+Попробуйте разные значения e и n.
+"""
+n = int(input("введите n:"))
+e = int(input("введите e:"))
+count = 2
+sum_p = 0
+while count <= n:
+    if count % e == 0:
+        count +=2
+        continue
+    sum_p += count
+    count += 2
+    print(sum_p) 
+print('')
+print(sum_p) 
